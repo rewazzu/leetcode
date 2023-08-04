2035d2c88df332caf028108e220bbb8bb09f4e8f
Hamza Shaikh <53708485+rewazzu@users.noreply.github.com>
2023-08-04 13:58:22 -0700

    Create ProductOfArrayExceptSelf.py

---
+++ Arrays and Hashing/ProductOfArrayExceptSelf.py
@@ -0,0 +1,25 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [[] for x in range(len(nums))]
+        postfix = [[] for x in range(len(nums))]
+        result = [[] for x in range(len(nums))]
+
+        for index, num in enumerate(nums):
+            if index == 0:
+                value = 1
+            else:
+                value = prefix[index - 1] * nums[index - 1]
+            prefix[index] = value
+
+        for index in range(len(nums) - 1, -1, -1):
+            if index == len(nums) - 1:
+                value = 1
+            else:
+                value = postfix[index + 1] * nums[index + 1]
+            postfix[index] = value
+
+        for index in range(len(nums)):
+            value = prefix[index] * postfix[index]
+            result[index] = value
+
+        return result
