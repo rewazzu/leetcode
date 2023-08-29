8a0010533460481659965cf667e55e512db26baa
Hamza Shaikh <53708485+rewazzu@users.noreply.github.com>
2023-08-29 12:25:16 -0700

    Create findtheDuplicateNumber.py

---
+++ Linked List/findtheDuplicateNumber.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[nums[0]]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        slow = 0
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
+
+'''Commentary: 
+This solution uses Floyd's algorithm. 
+We treat the array as a linked list. There is a cycle in the linked list'''
