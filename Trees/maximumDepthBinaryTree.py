841eced6ba1d607129a6a7e0dca820d3fb1176df
Hamza Shaikh <53708485+rewazzu@users.noreply.github.com>
2023-08-30 13:15:17 -0700

    Create maximumDepthBinaryTree.py

---
+++ Trees/maximumDepthBinaryTree.py
@@ -0,0 +1,18 @@
+def dfs(root):
+    left_depth = 1
+    right_depth = 1
+    if root.left:
+        left_depth = dfs(root.left) + 1
+    if root.right:
+        right_depth = dfs(root.right) +1
+    return max(left_depth, right_depth)
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        depth = dfs(root)
+        return depth
+
+
+'''Commentary: Simple DFS '''
